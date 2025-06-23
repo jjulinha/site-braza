a911203874350da227909a08ac4369a0afece5f9
julia <111931850+jjulinha@users.noreply.github.com>
2025-06-23 15:29:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -39,17 +39,18 @@ if (splash && video) {
   let lastScrollTop = 0;
   const header = document.querySelector('header');
 
-  window.addEventListener('scroll', function () {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop && scrollTop > 100) {
-      header.classList.add('header-hidden');
-    } else {
-      header.classList.remove('header-hidden');
-    }
-    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-  });
-
-  // Scroll Reveal
+  if (header) {
+    window.addEventListener('scroll', function () {
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      if (scrollTop > lastScrollTop && scrollTop > 100) {
+        header.classList.add('header-hidden');
+      } else {
+        header.classList.remove('header-hidden');
+      }
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    });
+  }
+// Scroll Reveal
   const revealElements = document.querySelectorAll('.reveal');
   const revealObserver = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
